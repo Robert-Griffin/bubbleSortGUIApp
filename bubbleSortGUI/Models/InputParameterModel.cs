61e4b6379a9b47370407e154ca0e83e3d1eb427b
Robert Griffin <Robert.griffin0908@gmail.com>
2022-10-30 21:40:17 -0400

    Inital commit

---
+++ bubbleSortGUI/Models/InputParameterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubbleSortGUI.Models
+{
+    public class InputParameterModel
+    {
+        public List<string> inputStrings;
+
+        public InputParameterModel()
+        {
+            inputStrings = new List<string>() {"A", "B", "C", "D", "E", "F", "G" };
+            
+        }
+
+       
+    }
+}
---
+++ bubbleSortGUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace bubbleSortGUI.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _leftString;
+        public string LeftString
+        {
+            get
+            {
+                return _leftString;
+            }
+            set
+            {
+                LeftString = value;
+                OnPropertyChanged(nameof(LeftString));
+            }
+        }
+
+        private string _rightString;
+
+        public string RightString
+        {
+            get 
+            {
+                return _rightString;
+            }
+            set
+            {
+                _rightString = value;
+                OnPropertyChanged(nameof(RightString));
+            }
+        }
+
+        public ICommand LeftSelectCommand { get; }
+        public ICommand RightSelectCommand { get; }
+    }
+}
