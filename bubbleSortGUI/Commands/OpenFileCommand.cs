fb2619756c0ba385ffa4dc178e4d96a766d8bedb
Robert Griffin <Robert.griffin0908@gmail.com>
2022-11-07 07:01:04 -0500

    style: Updated line spacing, made codebase more uniform.

--- bubbleSortGUI/Commands/OpenFileCommand.cs
+++ bubbleSortGUI/Commands/OpenFileCommand.cs
@@ -22,21 +22,15 @@ namespace bubbleSortGUI.Commands
 
         public void Execute(object parameter)
         {
-
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
-                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.InitialDirectory = "c:\\Downloads";
                 openFileDialog.Filter = "csv files (.csv)|*.csv";
-                openFileDialog.RestoreDirectory = true;
+                openFileDialog.RestoreDirectory = false;
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    //Get the path of specified file
                     string filePath = openFileDialog.FileName;
-
-                    //Read the contents of the file into a stream
-                    var fileStream = openFileDialog.OpenFile();
-
                     _readingCsv = true;
                     CanExecuteChanged.Invoke(this, EventArgs.Empty);
                     MainViewModel.ReadCSV(filePath);
